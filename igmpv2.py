92dd1e57b6278c9594b9bd5deadea5fcafeb4a5d
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-04-29 12:11:00 +0200

    Update igmpv2.py

--- igmpv2.py
+++ igmpv2.py
@@ -1,4 +1,4 @@
-# Envia 99 paquetes IGMPv2 con IP origen 192.168.1.201
+# Envia 99 paquetes IGMPv2 con IP origen 192.168.1.42
 # (c) hackingyseguridad.com 2018
 # https://github.com/levigross/Scapy/blob/master/scapy/contrib/igmp.py
 #    The function adjusts the IP header based on conformance rules 
@@ -12,7 +12,21 @@
 from scapy.all import *
 from scapy.contrib.igmp import IGMP
 eth = Ether()
-iph = IP(src='192.168.1.201', dst='224.0.0.1', proto=2)
+
+# Send General Group Query to 224.0.0.1 (all systems)
+iph = IP(src='192.168.1.42', dst='224.0.0.1', proto=2)
 igmp = IGMP(type=0x11, gaddr='0.0.0.0', mrtime=20)
 igmp.igmpize(iph,eth)
-sendp(eth/iph/igmp, iface="eth0", count=99)
+sendp(eth/iph/igmp, iface="eth1", count=1)
+
+# Send Leave Group to 224.0.0.2 (all routers)
+iph = IP(src='192.168.1.42', dst='224.0.0.2', proto=2)
+igmp = IGMP(type=0x17, gaddr='239.0.0.58', mrtime=20)
+igmp.igmpize(iph,eth)
+sendp(eth/iph/igmp, iface="eth1", count=1)
+
+# Send reports/joins to the group address
+iph = IP(src='192.168.1.42', dst='239.0.0.59', proto=2)
+igmp = IGMP(type=0x16, gaddr='239.0.0.59', mrtime=20)
+igmp.igmpize(iph,eth)
+sendp(eth/iph/igmp, iface="eth1", count=1)
