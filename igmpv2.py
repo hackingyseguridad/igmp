903b5750331198cfae1ae0236f3836383a9f0f68
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-04-29 11:39:51 +0200

    Update igmpv2.py

--- igmpv2.py
+++ igmpv2.py
@@ -1,6 +1,13 @@
 # Envia 99 paquetes IGMPv2 con IP origen 192.168.1.201
 # (c) hackingyseguridad.com 2018
 # https://github.com/levigross/Scapy/blob/master/scapy/contrib/igmp.py
+#    The function adjusts the IP header based on conformance rules 
+#    and the group address encoded in the IGMP message.
+#    The rules are:
+#    1. Send General Group Query to 224.0.0.1 (all systems)
+#    2. Send Leave Group to 224.0.0.2 (all routers)
+#    3a.Otherwise send the packet to the group address
+#    3b.Send reports/joins to the group address
 #!/usr/bin/env python
 from scapy.all import *
 from scapy.contrib.igmp import IGMP
